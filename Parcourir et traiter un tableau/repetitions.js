5ce3b469d75a0945b2c30ed32180feb7691c7931
Brahim Medkour <medkourbrahim@gmail.com>
2026-09-09 16:10:46 +0100

    repitition

--- Parcourir et traiter un tableau/repetitions.js
+++ Parcourir et traiter un tableau/repetitions.js
@@ -1,17 +1,57 @@
 let nombres = [4, 7, 2, 7, 9, 4, 5];
-let repetition=[];
+let repetition = [];
 
-for(let i=0 ; i < nombres.length ; i++){
+for (let i = 0; i < nombres.length; i++) {
 
-    for(let a=0 ; a < nombres.length ; a++){
-      
-        if(nombres[i] == nombres[a] && i!=a){
+    for (let a = 0; a < nombres.length; a++) {
 
-            
+        if (nombres[i] == nombres[a] && i != a) {
 
-            repetition.push(nombres[i]);
+            let existe = false;
+
+            // N9elbo f repetition
+            for (let b = 0; b < repetition.length; b++) {
+
+                if (nombres[i] == repetition[b]) {
+                    existe = true;
+                }
+            }
+
+            // Ila mazal ma kaynch
+            if (existe == false) {
+                repetition.push(nombres[i]);
+            }
         }
     }
-  
 }
-console.log(repetition);
+
+    console.log(repetition);
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let nombres = [4, 7, 2, 7, 9, 4, 5];
+// let repetition=[];
+
+// for(let i=0 ; i < nombres.length ; i++){
+
+//     for(let a=0 ; a < nombres.length ; a++){
+      
+//         if(nombres[i] == nombres[a] && i!=a){
+
+//             repetition.push(nombres[i]);
+//         }
+//     }
+  
+// }
+// console.log(repetition);
